f0413e3766c217b510aff7badb85df1c7f476a01
Andreas Tzionis <andy0n7@gmail.com>
2023-02-04 19:59:21 +0200

    updated destack version

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-import 'grapesjs/dist/css/grapes.min.css'
+import 'destack/build/browser/index.css'
 export { getStaticProps } from 'destack/build/server'
 export { ContentProvider as default } from 'destack'
